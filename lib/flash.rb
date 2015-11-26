ec9d338bb280d8dc32175d8a0e0a9c2913ebbaf9
Ryan Samp <sampryan@gmail.com>
2015-11-25 18:01:36 -0800

    Being Flash

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,15 +1,30 @@
+require 'json'
+
 class Flash
-
   def initialize(req)
-    if req.cookies.has_key?("_rails_lite_app")
-      @cookies = JSON.parse(req.cookies["_rails_lite_app"])
+    @flash_cookies = {}
+    if req.cookies.has_key?("_flash")
+      c = req.cookies["_flash"].map { |key, val| {key.to_s => val} }.flatten
+
+      @flash_cookies = JSON.parse(req.cookies["_flash"])
     end
-    flash = {}
-    flash_now = {}
   end
 
-  def store_flash(res)
+  def [](key)
+    @flash_cookies[key.to_s]
+  end
+
+  def []=(key, val)
+    @flash_cookies[key.to_s] = [val, 1]
+  end
+
+  def now
 
   end
 
+  def store_flash_cookies(res)
+    @flash_cookies.map { |key, val| [key.to_s, val] }
+    json_flash_cookies = @flash_cookies.to_json
+    res.set_cookie("_flash", {path: "/", value: json_flash_cookies})
+  end
 end
